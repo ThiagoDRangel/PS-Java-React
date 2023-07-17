114b225d840321048f546500a93a6775e2fb6964
ThiagoDRangel <info.tec.campos@gmail.com>
2023-07-16 21:44:53 -0300

    Modificações no Cors

--- src/main/java/br/com/banco/BancoApplication.java
+++ src/main/java/br/com/banco/BancoApplication.java
@@ -18,7 +18,7 @@ public class BancoApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:5173");
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
             }
         };
     }
--- src/main/java/br/com/banco/config/CorsConfiguration.java
+++ src/main/java/br/com/banco/config/CorsConfiguration.java
@@ -8,9 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry.addMapping("/api/transferencias")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+                .allowedMethods("GET")
+                .allowedHeaders("Content-Type");
     }
 
 }
